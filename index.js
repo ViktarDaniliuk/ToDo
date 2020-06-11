d44da0580a065fbab3a8304b9d8a5fe21e55a37d
ViktarDaniliuk <daniliukviktar@gmail.com>
2020-06-11 23:25:03 +0200

    index: add hendelbars

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const config = require('./config/key');
+const exphbs = require('express-handlebars');
 
 const PORT = process.env.PORT || 5050;
 
 const app = express();
+
+const hbs = exphbs.create({
+   defaultLayout: 'main',
+   extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
 async function start() {
    try {
