dc026d909cae87687054b6f7de4e11cca9d26f48
ViktarDaniliuk <daniliukviktar@gmail.com>
2020-06-12 00:11:01 +0200

    hbs: add title and active page

--- routes/todos.js
+++ routes/todos.js
@@ -2,11 +2,17 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-   res.render('index');
+   res.render('index', {
+      title: 'ToDo List',
+      isIndex: true
+   });
 });
 
 router.get('/create', (req, res) => {
-   res.render('create');
+   res.render('create', { 
+      title: 'Create To Do',
+      isCreate: true
+   });
 });
 
 module.exports = router;
