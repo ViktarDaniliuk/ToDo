f8f6e818c911f16adea7d95cb681b9f7acafa5bd
ViktarDaniliuk <daniliukviktar@gmail.com>
2020-06-13 21:53:05 +0200

    fix(todos.js): fix route '/'

--- routes/todos.js
+++ routes/todos.js
@@ -1,15 +1,18 @@
 const { Router } = require('express');
 const Todo = require('../models/todo');
 const router = Router();
+const fs = require('fs');
+const todo = require('../models/todo');
 
 router.get('/', async (req, res) => {
-   const todos = await Todo.find({});
+   const todos = await Todo.find();
 
-   res.render('index', {
+   const data = {
       title: 'ToDo List',
       isIndex: true,
-      todos: todos
-   });
+      todos: JSON.parse(JSON.stringify(todos))
+   }
+   res.render('index', data);
 });
 
 router.get('/create', (req, res) => {
@@ -17,6 +20,16 @@ router.get('/create', (req, res) => {
       title: 'Create To Do',
       isCreate: true
    });
+});
+
+router.post('/create', async (req, res) => {
+   const todo = new Todo({
+      title: req.body.title
+   });
+
+   await todo.save();
+
+   res.redirect('/');
 });
 
 module.exports = router;
